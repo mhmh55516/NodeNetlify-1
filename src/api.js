99cff9c6bf4920d3ad8ebcc12ce3b9b9c97e98f1
unknown <raj.ankur8@gmail.com>
2020-08-19 21:12:23 +0530

    changes for deploy Node

---
+++ src/api.js
@@ -0,0 +1,16 @@
+const express=require('express');
+const serverless = require('serverless-http');
+const cors = require('cors');
+const app = express();
+app.use(cors);
+const router = express.Router();
+
+router.get('/',(req,res)=>{
+    res.json({
+        'hello':'hi from node server'
+    })
+})
+
+app.use('/.netlify/functions/api',router);
+module.exports = app;
+module.exports.handler = serverless(app);
