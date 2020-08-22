9eaecb2c8a80954906d23131fbb14bb0056a6435
unknown <raj.ankur8@gmail.com>
2020-08-22 13:02:49 +0530

    updating files

--- src/api.js
+++ src/api.js
@@ -2,15 +2,16 @@ const express=require('express');
 const serverless = require('serverless-http');
 const cors = require('cors');
 const app = express();
-app.use(cors);
+//app.use(cors);
 const router = express.Router();
 
 router.get('/',(req,res)=>{
     res.json({
         'hello':'hi from node server'
+        
     })
 })
 
 app.use('/.netlify/functions/api',router);
-module.exports = app;
+//module.exports = app;
 module.exports.handler = serverless(app);
